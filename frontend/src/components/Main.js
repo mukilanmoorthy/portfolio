7fd83d859cd8d1594949d450ed0507402efdf7cd
mukilanmoorthy <mukilan93421@gmail.com>
2025-06-09 08:40:44 +0530

    change

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -15,17 +15,26 @@ background: ${props => props.theme.body};
 width: 100vw;
 height: 100vh;
 overflow:hidden;
-
 position: relative;
 
 h2,h3,h4,h5,h6{
-  font-family:'Karla', sans-serif ;
-  font-weight:500;
+    font-family:'Karla', sans-serif ;
+    font-weight:500;
+}
+
+@media (max-width: 768px) {
+    h2,h3,h4,h5,h6 {
+        font-size: 1.1rem;
+    }
 }
 `
 
 const Container = styled.div`
 padding: 2rem;
+
+@media (max-width: 768px) {
+    padding: 1rem;
+}
 `
 
 const Contact = styled.a`
@@ -35,6 +44,12 @@ top: 2rem;
 right: calc(1rem + 2vw);
 text-decoration: none;
 z-index:1;
+
+@media (max-width: 768px) {
+    top: 1rem;
+    right: 1rem;
+    font-size: 0.9rem;
+}
 `
 const BLOG = styled(NavLink)`
 color: ${props => props.theme.text};
@@ -44,16 +59,25 @@ right: calc(1rem + 2vw);
 transform: rotate(90deg) translate(-50%, -50%);
 text-decoration: none;
 z-index:1;
+
+@media (max-width: 768px) {
+    right: 0.5rem;
+    font-size: 0.9rem;
+}
 `
 const WORK = styled(NavLink)`
 color: ${props => props.click ? props.theme.body : props.theme.text};
-
 position: absolute;
 top: 50%;
 left: calc(1rem + 2vw);
 transform: translate(-50%, -50%) rotate(-90deg) ;
 text-decoration: none;
 z-index:1;
+
+@media (max-width: 768px) {
+    left: 0.5rem;
+    font-size: 0.9rem;
+}
 `
 
 const BottomBar = styled.div`
@@ -62,28 +86,40 @@ bottom: 1rem;
 left: 0;
 right: 0;
 width: 100%;
-
 display: flex;
 justify-content: space-evenly;
+
+@media (max-width: 768px) {
+    bottom: 0.5rem;
+    font-size: 0.9rem;
+}
 `
 
 const ABOUT = styled(NavLink)`
 color: ${props => props.click ? props.theme.body : props.theme.text};
 text-decoration: none;
 z-index:1;
+
+@media (max-width: 768px) {
+    font-size: 0.9rem;
+}
 `
 const SKILLS = styled(NavLink)`
 color: ${props => props.theme.text};
 text-decoration: none;
 z-index:1;
+
+@media (max-width: 768px) {
+    font-size: 0.9rem;
+}
 `
 
 const rotate = keyframes`
 from{
-    transform: rotate(0);
+        transform: rotate(0);
 }
 to{
-    transform: rotate(360deg);
+        transform: rotate(360deg);
 }
 `
 
@@ -104,12 +140,29 @@ align-items: center;
 transition: all 1s ease;
 
 &>:first-child{
-    animation: ${rotate} infinite 1.5s linear;
+        animation: ${rotate} infinite 1.5s linear;
+        width: ${props => props.click ? '80px' : '120px'};
+        height: ${props => props.click ? '80px' : '120px'};
 }
 
 &>:last-child{
-    display: ${props => props.click ? 'none' :'inline-block'  };
-    padding-top: 1rem;
+        display: ${props => props.click ? 'none' :'inline-block'  };
+        padding-top: 1rem;
+        font-size: 1rem;
+}
+
+@media (max-width: 768px) {
+    top: ${props => props.click ? '90%' :'50%'  };
+    left: ${props => props.click ? '90%' :'50%'  };
+
+    &>:first-child{
+        width: ${props => props.click ? '50px' : '80px'};
+        height: ${props => props.click ? '50px' : '80px'};
+    }
+    &>:last-child{
+        font-size: 0.8rem;
+        padding-top: 0.5rem;
+    }
 }
 `
 
@@ -123,6 +176,10 @@ width: ${props => props.click ? '50%' : '0%'};
 height: ${props => props.click ? '100%' : '0%'};
 z-index:1;
 transition: height 0.5s ease, width 1s ease 0.5s;
+
+@media (max-width: 768px) {
+    width: ${props => props.click ? '60%' : '0%'};
+}
 `
 
 
